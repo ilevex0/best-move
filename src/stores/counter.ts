5d85b63a7386b17f79d49415488e232bd29e56b8
Pedro <contactilevex@gmail.com>
2026-07-26 01:46:31 -0300

    add taskStore and Navbar component

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,12 +1,13 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
+export const useTaskStore = defineStore('task', () => {
+  const tasks = ref([
+    {
+      title: "Test",
+      time: "00:00"
+    }
+  ])
 
-  return { count, doubleCount, increment }
+  return { tasks }
 })
